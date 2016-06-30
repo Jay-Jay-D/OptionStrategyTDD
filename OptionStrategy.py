569560b8e08a19740db20c8cd364a94b52866114
Jay-Jay-D <jjdambrosio@gmail.com>
2016-06-30 08:11:16 -0300

    OptionOperation parse data from df_contracts when instantiated using contract data. Status ITM/OTM added

--- OptionStrategy.py
+++ OptionStrategy.py
@@ -7,23 +7,61 @@ class OptionType(Enum):
     Put = 1
 
 
+class OptionStatus(IntEnum):
+    OTM = 0
+    ITM = 1
+
+
 class Operation(IntEnum):
     Sell = -1
     Buy = 1
 
 
 class OptionOperation(object):
-    def __init__(self, option_type, operation, strike_price, premium):
-        self.ConId = None
-        self.option_type = option_type
+    def __init__(self, operation, premium, option_type, strike_price):
         self.operation = operation
-        self.strike_price = strike_price
         self.premium = premium
+        self.option_type = option_type
+        self.strike_price = strike_price
+        self.ConId = None
+        self.underlying_asset = None
 
     @classmethod
-    def from_ConId(cls, ConID, operation, premium, df_contracts):
+    def from_contract_description(cls, contracts: pd.DataFrame, operation, premium, option_type=None, strike_price=None,
+                                  underlying_asset=None):
+        cls.operation = operation
+        cls.premium = premium
+        queries = []
+
+        if option_type is not None:
+            if option_type == OptionType.Call:
+                queries.append("Right=='C'")
+            elif option_type == OptionType.Put:
+                queries.append("Right=='P'")
+
+        if strike_price is not None:
+            queries.append("Strike==" + str(strike_price))
+
+        if underlying_asset is not None:
+            queries.append("Symbol=='{}'".format(underlying_asset))
+
+        query = None
+        for q in queries:
+            if query is None:
+                query = q
+            else:
+                query = query + " and " + q
+
+        selected_contract = contracts.query(query)
+        if selected_contract.shape[0] > 1:
+            raise ValueError()
+        else:
+            return cls.from_ConId(contracts, selected_contract.index[0], option_type, premium)
+
+    @classmethod
+    def from_ConId(cls, contracts: pd.DataFrame, ConID, operation, premium):
         try:
-            rigth = df_contracts.ix[ConID, 'Right']
+            rigth = contracts.ix[ConID, 'Right']
         except KeyError:
             raise KeyError('The ConId does not exist in the contract JSON file.')
 
@@ -34,11 +72,12 @@ class OptionOperation(object):
         else:
             raise ValueError('The ConId is not an option.')
 
+        cls.ConId = ConID
         cls.operation = operation
-        cls.strike_price = df_contracts.ix[198003980, 'Strike']
+        cls.strike_price = contracts.ix[ConID, 'Strike']
         cls.premium = premium
+        cls.underlying_asset = contracts.ix[ConID, 'Symbol']
         return cls
-
 
     def value_at(self, price):
         if self.option_type == OptionType.Call:
@@ -47,3 +86,6 @@ class OptionOperation(object):
             value = - self.premium if price <= self.strike_price else (price - self.strike_price - self.premium)
         return value * self.operation.value
 
+    def status_at(self, price):
+        if self.value_at(price) >= 0: return OptionStatus.ITM
+        else: return OptionStatus.OTM
--- tests.py
+++ tests.py
@@ -7,6 +7,7 @@ from OptionStrategy import *
 path_to_contracts_json = '/mnt/HDD200GB/Algorithmic Trading/OptionStrategy/testing_files/Contracts.json'
 df_contracts = pd.read_json(path_to_contracts_json).dropna(axis=1, how='all').set_index('ConId')
 
+# region Passed Test
 option_valuation_test_cases = (("comment", "operation", "option_type", "expected_values_formula"),
                                [
                                    ('Buy a Call @150', 1, 0,
@@ -19,6 +20,19 @@ option_valuation_test_cases = (("comment", "operation", "option_type", "expected
                                     '[ 10 if price <= 150 else (150 - price + 10) for price in range(100, 200, 5)]'),
                                ])
 
+option_status_test_cases = (("comment", "operation", "option_type", "price", "correct_status"),
+                            [
+                                ('Long  Call @150 ITM at 120', 1, 0, 120, 1),
+                                ('Long  Call @150 OTM at 170', 1, 0, 170, 0),
+                                ('Short Call @150 ITM at 170', -1, 0, 170, 1),
+                                ('Short Call @150 OTM at 139', -1, 0, 139, 0),
+                                ('Long  Put  @150 ITM at 160', 1, 1, 160, 1),
+                                ('Long  Put  @150 OTM at 151', 1, 1, 151, 0),
+                                ('Short Put  @150 ITM at 160', -1, 1, 160, 1),
+                                ('Short Put  @150 OTM at 170', -1, 1, 170, 0),
+
+                            ])
+
 
 @pytest.mark.parametrize(*option_valuation_test_cases)
 def test_options_are_correctly_evaluated(comment, operation, option_type, expected_values_formula):
@@ -27,7 +41,7 @@ def test_options_are_correctly_evaluated(comment, operation, option_type, expect
     expected_value = eval(expected_values_formula)
     operation = Operation(operation)
     option_type = OptionType(option_type)
-    option = OptionOperation(option_type, operation, strike_price=150, premium=10)
+    option = OptionOperation(operation=operation, premium=10, option_type=option_type, strike_price=150)
     # Act
     for price in range(100, 200, 5):
         actual_values.append(option.value_at(price))
@@ -35,24 +49,53 @@ def test_options_are_correctly_evaluated(comment, operation, option_type, expect
     assert expected_value == actual_values
 
 
+@pytest.mark.parametrize(*option_status_test_cases)
+def test_option_status_is_correctly_estimated(comment, operation, option_type, price, correct_status):
+    operation = Operation(operation)
+    option_type = OptionType(option_type)
+    option = OptionOperation(operation=operation, premium=10, option_type=option_type, strike_price=150)
+
+    assert option.status_at(price) == OptionStatus(correct_status)
+
+
 def test_when_a_non_option_is_taken_using_ConId_then_throws_ValueError_exception():
     # Try to add a Future
     with pytest.raises(ValueError):
-        option = OptionOperation.from_ConId(ConID=187532577, df_contracts=df_contracts,
+        option = OptionOperation.from_ConId(contracts=df_contracts, ConID=187532577,
                                             operation=Operation.Buy, premium=10)
 
 
 def test_when_ConId_is_not_present_in_Contract_JSON_then_throws_KeyError_exception():
     with pytest.raises(KeyError):
-        option = OptionOperation.from_ConId(ConID=198003981, df_contracts=df_contracts,
+        option = OptionOperation.from_ConId(contracts=df_contracts, ConID=198003981,
                                             operation=Operation.Buy, premium=10)
 
 
 def test_when_an_option_is_taken_using_ConId_then_its_data_is_correctly_parsed():
-    option = OptionOperation.from_ConId(ConID=198003980, df_contracts=df_contracts, operation=Operation.Buy, premium=10)
+    option = OptionOperation.from_ConId(contracts=df_contracts, ConID=198003980,
+                                        operation=Operation.Buy, premium=10)
 
     assert option.option_type == OptionType.Put
     assert option.strike_price == 2070
+    assert option.ConId == 198003980
+
+
+def test_when_option_is_not_instantiated_with_ConId_and_the_given_parameters_are_not_enough_to_pick_just_one_thrown_error():
+    with pytest.raises(ValueError):
+        option = OptionOperation.from_contract_description(df_contracts, operation=Operation.Buy, premium=10,
+                                                           option_type=OptionType.Put)
+
+
+def test_when_option_is_correctly_instantiated_with_out_ConId_then_its_data_its_correctly_parsed():
+    option = OptionOperation.from_contract_description(df_contracts, operation=Operation.Buy, premium=10,
+                                                       option_type=OptionType.Put, strike_price=2000)
+    assert option.ConId == 198003948
+    assert option.underlying_asset == 'ES'
+
+
+# endregion
+
+
 
 
 class OptionStrategyTests(unittest.TestCase):
