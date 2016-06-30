9b60f6a58302f5eddbe77a5400bbcfffde235a73
Jay-Jay-D <jjdambrosio@gmail.com>
2016-06-29 22:29:34 -0300

    Contracts.json parsed to DataFrame. OptionOperation parse data from df_contracts when instantiated using ConId

--- OptionStrategy.py
+++ OptionStrategy.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from enum import Enum, IntEnum
 
 
@@ -12,15 +13,37 @@ class Operation(IntEnum):
 
 
 class OptionOperation(object):
-    def __init__(self, option_type, operation, strike_price, premiun):
+    def __init__(self, option_type, operation, strike_price, premium):
+        self.ConId = None
         self.option_type = option_type
         self.operation = operation
         self.strike_price = strike_price
-        self.premiun = premiun
+        self.premium = premium
+
+    @classmethod
+    def from_ConId(cls, ConID, operation, premium, df_contracts):
+        try:
+            rigth = df_contracts.ix[ConID, 'Right']
+        except KeyError:
+            raise KeyError('The ConId does not exist in the contract JSON file.')
+
+        if rigth == 'C':
+            cls.option_type = OptionType.Call
+        elif rigth == 'P':
+            cls.option_type = OptionType.Put
+        else:
+            raise ValueError('The ConId is not an option.')
+
+        cls.operation = operation
+        cls.strike_price = df_contracts.ix[198003980, 'Strike']
+        cls.premium = premium
+        return cls
+
 
     def value_at(self, price):
         if self.option_type == OptionType.Call:
-            value = - self.premiun if price >= self.strike_price else (self.strike_price - self.premiun - price)
+            value = - self.premium if price >= self.strike_price else (self.strike_price - self.premium - price)
         elif self.option_type == OptionType.Put:
-            value = - self.premiun if price <= self.strike_price else (price - self.strike_price - self.premiun)
+            value = - self.premium if price <= self.strike_price else (price - self.strike_price - self.premium)
         return value * self.operation.value
+
--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
+import pandas as pd
 import unittest
-
-import numpy as np
 import pytest
 
 from OptionStrategy import *
+
+path_to_contracts_json = '/mnt/HDD200GB/Algorithmic Trading/OptionStrategy/testing_files/Contracts.json'
+df_contracts = pd.read_json(path_to_contracts_json).dropna(axis=1, how='all').set_index('ConId')
 
 option_valuation_test_cases = (("comment", "operation", "option_type", "expected_values_formula"),
                                [
@@ -25,20 +27,35 @@ def test_options_are_correctly_evaluated(comment, operation, option_type, expect
     expected_value = eval(expected_values_formula)
     operation = Operation(operation)
     option_type = OptionType(option_type)
-    option = OptionOperation(option_type, operation, strike_price=150, premiun=10)
-
+    option = OptionOperation(option_type, operation, strike_price=150, premium=10)
     # Act
     for price in range(100, 200, 5):
         actual_values.append(option.value_at(price))
-
     # Assert
     assert expected_value == actual_values
 
 
-class OptionStrategyTests(unittest.TestCase):
-    def when_an_option_is_taken_then_its_data_is_correctly_parsed(self):
-        self.assertTrue(False)
+def test_when_a_non_option_is_taken_using_ConId_then_throws_ValueError_exception():
+    # Try to add a Future
+    with pytest.raises(ValueError):
+        option = OptionOperation.from_ConId(ConID=187532577, df_contracts=df_contracts,
+                                            operation=Operation.Buy, premium=10)
 
+
+def test_when_ConId_is_not_present_in_Contract_JSON_then_throws_KeyError_exception():
+    with pytest.raises(KeyError):
+        option = OptionOperation.from_ConId(ConID=198003981, df_contracts=df_contracts,
+                                            operation=Operation.Buy, premium=10)
+
+
+def test_when_an_option_is_taken_using_ConId_then_its_data_is_correctly_parsed():
+    option = OptionOperation.from_ConId(ConID=198003980, df_contracts=df_contracts, operation=Operation.Buy, premium=10)
+
+    assert option.option_type == OptionType.Put
+    assert option.strike_price == 2070
+
+
+class OptionStrategyTests(unittest.TestCase):
     def test_when_buy_a_new_option_then_strategy_is_updated(self):
         self.assertTrue(False)
 
